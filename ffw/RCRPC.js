78a15ae2c0bc38074ecc1c1df954ecc59962b22e
AKalinich-Luxoft <AKalinich@luxoft.com>
2017-07-22 16:32:00 +0300

    Fixed GetInteriorVehicleData processing according to proposal

--- ffw/RCRPC.js
+++ ffw/RCRPC.js
@@ -233,7 +233,7 @@ FFW.RC = FFW.RPCObserver.create(
               return;
             }
 
-            var moduleType = request.params.moduleDescription.moduleType;
+            var moduleType = request.params.moduleType;
             var climateControlData = null;
             var radioControlData = null;
 
