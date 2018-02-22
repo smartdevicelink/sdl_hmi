281c05c616ecfc33eb0d3af489337381405ac103
jacobkeeler <jacob.keeler@livioradio.com>
2018-02-22 13:36:31 -0500

    Fix invalid data in GetWayPoints messages

--- ffw/NavigationRPC.js
+++ ffw/NavigationRPC.js
@@ -511,7 +511,7 @@ FFW.Navigation = FFW.RPCObserver.create(
      * @param {Object} data
      * @param {number} id
      */
-    wayPointSend: function(resultCode, data, id) {
+    wayPointSend: function(resultCode, data, id, appID) {
       if (resultCode == SDL.SDLModel.data.resultCode.SUCCESS &&
         data && id) {
         // send repsonse
@@ -519,6 +519,8 @@ FFW.Navigation = FFW.RPCObserver.create(
           'jsonrpc': '2.0',
           'id': id,
           'result': {
+            'code': resultCode, // type (enum) from SDL protocol
+            'appID': appID,
             'wayPoints': data,
             'method': 'Navigation.GetWayPoints'
           }
