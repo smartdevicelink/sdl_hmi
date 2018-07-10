a54ab7d5698f6b7033c6755aa47e3983e387b0b3
v-malko4 <vmalkov@luxoft.com>
2018-07-10 16:35:14 +0300

    Interior Vehicle Data Subscription Management and Data Caching in SDL
    Erase appId processing on the GIVD request
    at the next file:
    
    - ffw/RCRPC.js

--- ffw/RCRPC.js
+++ ffw/RCRPC.js
@@ -188,14 +188,6 @@ FFW.RC = FFW.RPCObserver.create(
           {
             Em.Logger.log('FFW.' + request.method + ' Request');
 
-            if (!this.consentedAppCheck(request)) {
-              this.sendError(
-                SDL.SDLModel.data.resultCode.REJECTED,
-                request.id, request.method
-              );
-              return;
-            }
-
             if (request.params.moduleData.radioControlData) {
               if (request.params.moduleData.radioControlData.radioEnable == null
                   && SDL.RadioModel.radioControlStruct.radioEnable == false) {
@@ -265,29 +257,10 @@ FFW.RC = FFW.RPCObserver.create(
           {
             Em.Logger.log('FFW.' + request.method + ' Request');
 
-            if (request.params.appID == undefined) {
-             this.sendError(
-               SDL.SDLModel.data.resultCode.INVALID_DATA, request.id,
-               request.method, 'appID parameter missing!'
-             );
-             return;
-            }
-
-            if (!this.consentedAppCheck(request)) {
-              this.sendError(
-                SDL.SDLModel.data.resultCode.REJECTED,
-                request.id, request.method
-              );
-              return;
-            }
-
             var moduleType = request.params.moduleType;
             var climateControlData = null;
             var radioControlData = null;
 
-            var app = SDL.SDLController.getApplicationModel(
-              request.params.appID
-            );
             if (moduleType === 'CLIMATE') {
               climateControlData = SDL.ClimateController.model.getClimateControlData();
             } else if (moduleType === 'RADIO') {
@@ -469,32 +442,5 @@ FFW.RC = FFW.RPCObserver.create(
         Em.Logger.log('FFW.RC.OnInteriorVehicleData Notification');
         FFW.RC.client.send(JSONMessage);
     },
-    /**
-     * Verification for consented apps
-     * HMI should reject secon unconsented app
-     * @param request
-     */
-    consentedAppCheck: function(request) {
-      var appID = request.params.appID;
-      var moduleType = null;
-      if (request.params.moduleDescription) {
-        moduleType = request.params.moduleDescription.moduleType;
-      } else if (request.params.moduleData) {
-        moduleType = request.params.moduleData.moduleType;
-      } else {
-        moduleType = request.params.moduleType;
-      }
-
-      var deviceName = SDL.SDLController.getApplicationModel(appID)
-        .deviceName;
-
-      if ((SDL.SDLModel.driverDeviceInfo &&
-        deviceName != SDL.SDLModel.driverDeviceInfo.name) ||
-        !SDL.SDLModel.reverseFunctionalityEnabled) {
-        return false;
-      }
-
-      return true;
-    }
   }
 );
