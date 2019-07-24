a68f82378c9c1c7035ba582649a932be7d0e2d4c
Collin <collin@livio.io>
2019-07-24 12:51:04 -0400

    omit cancelID when not used

--- ffw/UIRPC.js
+++ ffw/UIRPC.js
@@ -291,18 +291,18 @@ FFW.UI = FFW.RPCObserver.create(
           {
             var targetID = request.params.cancelID;
             var typeID = request.params.functionID;
-            
+
             if (typeID === 10 && SDL.InteractionChoicesView.active
-               && (targetID === -1 || targetID === SDL.InteractionChoicesView.cancelID)) {
+               && (targetID === undefined || targetID === SDL.InteractionChoicesView.cancelID)) {
               SDL.InteractionChoicesView.deactivate('ABORTED');
             } else if (typeID === 12 && SDL.AlertPopUp.active
-               && (targetID === -1 || targetID === SDL.AlertPopUp.cancelID)) {
+               && (targetID === undefined || targetID === SDL.AlertPopUp.cancelID)) {
               SDL.AlertPopUp.deactivate();
             } else if (typeID === 25 && SDL.ScrollableMessage.active
-               && (targetID === -1 || targetID === SDL.ScrollableMessage.cancelID)) {
+               && (targetID === undefined || targetID === SDL.ScrollableMessage.cancelID)) {
               SDL.ScrollableMessage.deactivate(true);
             } else if (typeID === 26 && SDL.SliderView.active
-               && (targetID === -1 || targetID === SDL.SliderView.cancelID)) {
+               && (targetID === undefined || targetID === SDL.SliderView.cancelID)) {
               SDL.SliderView.deactivate(true);
             } else {
               this.sendError(SDL.SDLModel.data.resultCode.IGNORED,
