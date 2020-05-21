cf05b6f0613203bf09f9674b71be6ec85c87c857
YarikMamykin <ymamykin@gmail.com>
2020-05-21 19:37:11 +0300

    Rework usage of DisplayMode selection view

--- app/view/home/controlButtons.js
+++ app/view/home/controlButtons.js
@@ -142,6 +142,7 @@ getCurrentDisplayModeClass: function() {
     default: return '';
   }
 },
+
 /**
  * HMI element Select with list of supported image mode
  */
@@ -149,37 +150,11 @@ getCurrentDisplayModeClass: function() {
     elementId: 'imageMode',
     classNames: 'imageModeSelect',
     contentBinding: 'SDL.SDLModel.data.imageModeList',
-    selection: 'Highlighted mode',
-    change:function(){
-      SDL.InfoAppsView.findNewApps.setMode(this.selection);
-      SDL.InfoAppsView.Asist911.setMode(this.selection);
-      SDL.InfoAppsView.vehicleHealthReport.setMode(this.selection);
-      SDL.InfoAppsView.getDeviceList.setMode(this.selection);
-      SDL.InfoView.leftMenu.items.servicesButton.setMode(this.selection);
-      SDL.InfoView.leftMenu.items.appsButton.setMode(this.selection);
-      SDL.InfoView.leftMenu.items.calendarButton.setMode(this.selection);
-      SDL.InfoView.leftMenu.items.goToCD.setMode(this.selection);
-      SDL.InfoView.leftMenu.items.travelLinkButton.setMode(this.selection);
-      SDL.InfoView.leftMenu.items.sdlButton.setMode(this.selection);
-      SDL.TurnByTurnView.nextTurnIconImage.setMode(this.selection);
-      SDL.TurnByTurnView.turnIconImage.setMode(this.selection);
-      SDL.InteractionChoicesView.set('imageMode',this.selection);
-      SDL.InteractionChoicesView.updateIcons();
-      if (SDL.SDLController.model) {
-        SDL.SDLController.model.setMode(this.selection);
-        length=SDL.OptionsView.commands.items.length;
-        var commands = SDL.SDLController.model.get('currentCommandsList');
-        for(var i=0;i<length;i++){
-          SDL.OptionsView.commands.items[i].type.prototype.setMode(this.selection);
-          if(commands[i].isTemplate){
-          SDL.OptionsView.commands.items[i].type.prototype.setMode(this.selection);
-          }
-        }
-        SDL.OptionsView.commands.refreshItems();
-      }
-    }
+    valueBinding: 'SDL.SDLModel.data.imageMode',
+    selection: 'Highlighted mode'
   }
 ),
+
 
   keyboard: SDL.Button.extend({
         classNames: ['keyboard', 'button'],
