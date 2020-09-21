6a4cf0935dcf31a35de66734e8942f67142d19ca
YarikMamykin <ymamykin@luxoft.com>
2020-09-21 15:19:47 +0300

    Remove unsed local variables

--- ffw/UIRPC.js
+++ ffw/UIRPC.js
@@ -1986,8 +1986,6 @@ FFW.UI = FFW.RPCObserver.create(
      */
     OnSystemContext: function(systemContextValue, appID, windowID) {
       Em.Logger.log('FFW.UI.OnSystemContext');
-      var allowedDepth = SDL.systemCapabilities.driverDistractionCapability.subMenuDepth-1;
-      var activeDepth = SDL.SDLController.model.get('currentMenuDepth')
       // send repsonse
       var JSONMessage = {
         'jsonrpc': '2.0',
