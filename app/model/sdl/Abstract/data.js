a72c89613e45d2a5e8a7da741eeb78fd5c3790a0
JackLivio <jack@livio.io>
2021-01-27 15:59:08 -0500

    Update data capabilities

--- app/model/sdl/Abstract/data.js
+++ app/model/sdl/Abstract/data.js
@@ -918,6 +918,30 @@ SDL.SDLModelData = Em.Object.create(
                   "characterSet": "UTF_8",
                   "width": 500,
                   "rows": 1
+                },
+                {
+                  'name': 'menuCommandSecondaryText',
+                  'characterSet': 'UTF_8',
+                  'width': 500,
+                  'rows': 1
+                },
+                {
+                  'name': 'menuCommandTertiaryText',
+                  'characterSet': 'UTF_8',
+                  'width': 500,
+                  'rows': 1
+                },
+                {
+                  'name': 'menuSubMenuSecondaryText',
+                  'characterSet': 'UTF_8',
+                  'width': 500,
+                  'rows': 1
+                },
+                {
+                  'name': 'menuSubMenuTertiaryText',
+                  'characterSet': 'UTF_8',
+                  'width': 500,
+                  'rows': 1
                 }
               ],
               'imageFields': [
@@ -1079,6 +1103,30 @@ SDL.SDLModelData = Em.Object.create(
                 },
                 {
                   'name': 'subtleAlertIcon',
+                  'imageTypeSupported': [
+                    'GRAPHIC_BMP',
+                    'GRAPHIC_JPEG',
+                    'GRAPHIC_PNG'
+                  ],
+                  'imageResolution': {
+                    'resolutionWidth': 105,
+                    'resolutionHeight': 65
+                  }
+                },
+                {
+                  'name': 'menuCommandSecondaryImage',
+                  'imageTypeSupported': [
+                    'GRAPHIC_BMP',
+                    'GRAPHIC_JPEG',
+                    'GRAPHIC_PNG'
+                  ],
+                  'imageResolution': {
+                    'resolutionWidth': 105,
+                    'resolutionHeight': 65
+                  }
+                },
+                {
+                  'name': 'menuSubMenuSecondaryImage',
                   'imageTypeSupported': [
                     'GRAPHIC_BMP',
                     'GRAPHIC_JPEG',
