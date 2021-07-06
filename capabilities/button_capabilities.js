677e97a1c964cd31bcfb26e81aa41cd97a906f3a
valerii <valera.malkof@gmail.com>
2021-07-06 14:40:57 +0300

    SDL-0192 Created separate file for button capabilites

---
+++ capabilities/button_capabilities.js
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2021, Ford Motor Company All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: ·
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. · Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. · Neither the name of the Ford Motor Company nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * @name SDL.ButtonCapability
+ * @desc Button capabilities
+ * @category Capability
+ * @filesource capabilities/button_capabilities.js
+ * @version 1.0
+ */
+
+SDL.ButtonCapability = [
+    {
+        name: 'PRESET_0',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_1',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_2',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_3',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_4',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_5',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_6',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_7',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_8',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PRESET_9',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'OK',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'PLAY_PAUSE',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'SEEKLEFT',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'SEEKRIGHT',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'TUNEUP',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'TUNEDOWN',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    },
+    {
+        name: 'CUSTOM_BUTTON',
+        shortPressAvailable: true,
+        longPressAvailable: true,
+        upDownAvailable: true
+    }
+]
