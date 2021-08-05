7174dcbdc6105f5690196d862574baaef63fa619
valerii <valera.malkof@gmail.com>
2021-08-05 13:15:32 +0300

    SDL-0192 SDL.ButtonCapability used in display_capabilities

--- capabilities/display_capabilities.js
+++ capabilities/display_capabilities.js
@@ -388,87 +388,7 @@ SDL.templateCapabilities = {
             },
             "numCustomPresetsAvailable": 10
         },
-        "buttonCapabilities": [{
-            "name": "PRESET_0",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_1",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_2",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_3",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_4",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_5",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_6",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_7",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_8",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_9",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "OK",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PLAY_PAUSE",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "SEEKLEFT",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "SEEKRIGHT",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "TUNEUP",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "TUNEDOWN",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }],
+        "buttonCapabilities": SDL.ButtonCapability.filter(button => button.name !== 'CUSTOM_BUTTON'),
         "softButtonCapabilities": [{
             "shortPressAvailable": true,
             "longPressAvailable": true,
@@ -847,82 +767,7 @@ SDL.templateCapabilities = {
             },
             "numCustomPresetsAvailable": 8
         },
-        "buttonCapabilities": [{
-            "name": "PRESET_0",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_1",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_2",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_3",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_4",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_5",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_6",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_7",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_8",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "PRESET_9",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "OK",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "SEEKLEFT",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "SEEKRIGHT",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "TUNEUP",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }, {
-            "name": "TUNEDOWN",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }],
+        "buttonCapabilities": SDL.ButtonCapability.filter(button => button.name !== 'CUSTOM_BUTTON'),
         "softButtonCapabilities": [{
             "shortPressAvailable": true,
             "longPressAvailable": true,
@@ -1264,12 +1109,7 @@ SDL.templateCapabilities = {
                 }
             }
         },
-        "buttonCapabilities": [{
-            "name": "CUSTOM_BUTTON",
-            "shortPressAvailable": true,
-            "longPressAvailable": true,
-            "upDownAvailable": true
-        }],
+        "buttonCapabilities": SDL.ButtonCapability.filter(button => button.name === 'CUSTOM_BUTTON'),
         "softButtonCapabilities": [{
             "shortPressAvailable": true,
             "longPressAvailable": true,
