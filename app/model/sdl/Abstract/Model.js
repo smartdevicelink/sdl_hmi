1897d6c226f7429266d84a1f2197fdd52d22dc4d
valerii <valera.malkof@gmail.com>
2021-09-15 14:41:43 +0300

    Fixed issue: HMI Uncaught SyntaxError after processing SetDisplayLayout(displayLayout = DEFAULT) for media and non-media apps

--- app/model/sdl/Abstract/Model.js
+++ app/model/sdl/Abstract/Model.js
@@ -626,7 +626,6 @@ SDL.SDLModel = Em.Object.extend({
         return model.appType.indexOf('WEB_VIEW') >= 0;
       }
       case 'NON-MEDIA':
-      case 'DEFAULT':
       {
         return true;
       }
