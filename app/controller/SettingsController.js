24d0d3d84044246d77a4f8ff557f82c915e42a71
Valerii Malkov <VMalkov@luxoft.com>
2022-01-13 22:49:53 +0200

    Fixed issue: crash after switch app during PHONE_CALL (#651)

--- app/controller/SettingsController.js
+++ app/controller/SettingsController.js
@@ -128,8 +128,11 @@ SDL.SettingsController = Em.Object.create(
         setTimeout(
           function() {
             SDL.SDLController.onEventChanged('phoneCall', false);
-            SDL.SDLController.getApplicationModel(appID).turnOnSDL(appID);
             SDL.SDLModel.data.phoneCallActive = false;
+            if(SDL.SDLController.model && SDL.SDLController.model.appID !== appID) {
+              return;
+            }
+            SDL.SDLController.getApplicationModel(appID).turnOnSDL(appID);
           }, 20000
         ); //Magic number - 5 seconds timeout for emulating conversation call
       } else {
